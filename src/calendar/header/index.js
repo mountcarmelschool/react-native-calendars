6619109b1a9c7818a53ad6663765365cc9ddc85e
mountcarmelschool <webmaster@mountcarmelschool.com>
2026-01-12 10:53:26 +0530

    React 19 support (#1)
    * removed defaultProps from calendar header component
    
    * upgraded lib version

--- src/calendar/header/index.js
+++ src/calendar/header/index.js
@@ -14,7 +14,7 @@ const CalendarHeader = forwardRef((props, ref) => {
     style: propsStyle,
     addMonth: propsAddMonth,
     month,
-    monthFormat,
+    monthFormat = 'MMMM yyyy',
     firstDay,
     hideDayNames,
     showWeekNumbers,
@@ -29,7 +29,7 @@ const CalendarHeader = forwardRef((props, ref) => {
     displayLoadingIndicator,
     customHeaderTitle,
     renderHeader,
-    webAriaLevel,
+    webAriaLevel = 1,
     testID,
     accessibilityElementsHidden,
     importantForAccessibility,
@@ -204,8 +204,3 @@ const CalendarHeader = forwardRef((props, ref) => {
 });
 export default CalendarHeader;
 CalendarHeader.displayName = 'CalendarHeader';
-CalendarHeader.defaultProps = {
-  monthFormat: 'MMMM yyyy',
-  webAriaLevel: 1,
-  arrowsHitSlop: 20
-};
